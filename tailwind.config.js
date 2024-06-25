3f0bb1d423f13fa0c95d3b13e63df8c9fbabb67f
Andrey Ostapovich <artvik48@gmail.com>
2024-06-25 19:22:44 +0300

    Добавил tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./web/components/*.vue", "./web/pages/*.vue"],
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- web/pages/CatalogPage.vue
+++ web/pages/CatalogPage.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <h1>Каталог товаров</h1>
-        <p>Тут будет каталог товаров, но это не точно</p>
+    <div class="container mx-auto p-4">
+      <h1 class="text-3xl font-bold mb-4">Каталог товаров</h1>
+      <p>Тут будет каталог товаров, но это не точно</p>
     </div>
 </template>
